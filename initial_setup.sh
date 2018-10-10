bb61c2ca2bd47f6a212ce864b8c6da38df57eaea
Nizam Arusada <nizam@styletheory.co>
2018-10-10 18:41:43 +0700

    update path

--- initial_setup.sh
+++ initial_setup.sh
@@ -4,9 +4,12 @@ mkdir ~/Documents/local-data/postgres
 mkdir ~/Documents/local-data/postgres/postgresdatabase
 mkdir ~/Documents/local-data/postgres/postgresmetabase
 
-sh /home/nizam/Downloads/git/data_analyst_kit/docker/setup/build_local.sh
-cd /home/nizam/Downloads/git/data_analyst_kit/docker/
-    && docker-compose up -d
-sleep 2m
+sh ~/Downloads/git/the-data-analyst-toolkit/docker/setup/build_local.sh
+cd ~/Downloads/git/the-data-analyst-toolkit/docker/
+docker-compose up -d
+docker ps
+sleep 15
+docker ps
+sleep 15
 echo "finish"
 exit 0
